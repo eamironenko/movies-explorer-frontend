89e4e636d8865375e605289de29ded0114badf0c
unknown <eam618334@yandex.ru>
2022-10-20 00:57:37 +0300

    feat: добавить компонент movies

---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,29 @@
+import './Movies.css';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import card1 from '../../images/card1.svg';
+import card2 from '../../images/card2.svg';
+import card3 from '../../images/card3.svg';
+import card4 from '../../images/card4.svg';
+import card5 from '../../images/card5.svg';
+import card6 from '../../images/card6.svg';
+import card7 from '../../images/card7.svg';
+import card8 from '../../images/card8.svg';
+import card9 from '../../images/card9.svg';
+import card10 from '../../images/card10.svg';
+import card11 from '../../images/card11.svg';
+import card12 from '../../images/card12.svg';
+import saveButton from '../../images/movie_save.svg'
+const cards = [];
+cards.push(card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12);
+
+function Movies() {
+    return (
+        <div className='movies'>
+            <SearchForm />
+            <MoviesCardList cards={cards} saveButton={saveButton} />
+        </div>
+    )
+}
+
+export default Movies;
